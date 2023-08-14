da73eadfc17d6c59ccab15515e3961fa72bd6bde
Diego Guerrero <105182864+iseka1@users.noreply.github.com>
2023-08-14 16:26:26 -0400

    Vacaciones se insertan en la data sobre desafios
    Version con 1 celda por mes, vacaciones reemplaza desafio en el mes aunque solo sea 1 dia

--- frontend/index.js
+++ frontend/index.js
@@ -14,56 +14,66 @@ function MyBlock() {
         margin: '4px 2px',
         cursor: 'pointer',
         borderRadius: '4px',
-      };
+    };
+
     const base = useBase();
-    const table = base.getTableByName('Team Memberships').getView("CurrentCSV"); // Replace with your table name
-    const records = useRecords(table);
+    const tableTM = base.getTableByName('Team Memberships').getView('CurrentCSV'); // Replace with your table name
+    const recordsTM = useRecords(tableTM);
+
+    const tableVacations = base.getTableByName('Vacations').getView('Grid view'); // Replace with your table name
+    const recordsVacations = useRecords(tableVacations);
 
     const exportToCSV = async () => {
         const months = [
             'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
             'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
         ];
-    
-        // Agrupar los registros por el valor de "Medusa"
-        const groupRecords = {};
-        records.forEach(record => {
+
+        // Create a mapping of Medusa to Team data
+        const medusaTeamMap = {};
+        recordsTM.forEach(record => {
             const medusa = record.getCellValueAsString('Medusa');
-            if (!groupRecords[medusa]) {
-                groupRecords[medusa] = [];
+            const team = record.getCellValueAsString('Team');
+            if (!medusaTeamMap[medusa]) {
+                medusaTeamMap[medusa] = {
+                    rol: record.getCellValueAsString('Charge for Pipeline (from People) (from Medusa)'),
+                    team: team,
+                    months: new Array(12).fill('')
+                };
             }
-            groupRecords[medusa].push(record);
+
+            const since = record.getCellValue('MonthSince');
+            const until = record.getCellValue('MonthUntil');
+
+            for (let month = since; month <= until; month++) {
+                medusaTeamMap[medusa].months[month - 1] = team;
+            }
         });
-    
+
+        recordsVacations.forEach(vacation => {
+            const medusa = vacation.getCellValueAsString('Medusa');
+            const since = vacation.getCellValue('MonthSince');
+            const until = vacation.getCellValue('MonthUntil');
+
+            for (let month = since; month <= until; month++) {
+                const teamData = medusaTeamMap[medusa];
+                if (teamData) {
+                    teamData.months[month - 1] = 'Vacation';
+                }
+            }
+        });
+
         const csvData = [
-            'Medusa,Rol,' + months.join(','), // Add months row
-            ...Object.entries(groupRecords).map(([medusa, records]) => {
-                const rol = records[0].getCellValueAsString('Charge for Pipeline (from People) (from Medusa)');
-                let values = [medusa, rol];   
-                for (let month = 1; month <= 12; month++) {
-                    const data = records.find(record => {
-                        const since = record.getCellValue('MonthSince');
-                        const until = record.getCellValue('MonthUntil');
-                        const daySince = record.getCellValueAsString('daySinceTeam');
-                        const datUntil = record.getCellValueAsString('dayUntilTeam');
-                        return month >= since && month <= until;
-                    });  
-                    if (data) {  
-                        
-                        
-                        const team = data.getCellValueAsString('Team');
-                        values.push(team);
-                    } else {
-                        values.push('');
-                    }
-                }    
+            ['Medusa', 'Rol', ...months], // Add header row
+            ...Object.entries(medusaTeamMap).map(([medusa, data]) => {
+                const values = [medusa, data.rol, ...data.months];
                 return values.join(',');
             })
         ].join('\n');
-    
+
         const csvBlob = new Blob([csvData], { type: 'text/csv' });
         const csvUrl = URL.createObjectURL(csvBlob);
-    
+
         const link = document.createElement('a');
         link.href = csvUrl;
         link.download = 'exported_data.csv';
@@ -74,7 +84,6 @@ function MyBlock() {
     };
 
     return (
-        
         <div>
             <button style={botonStyles} onClick={exportToCSV}>Export to CSV</button>
         </div>
