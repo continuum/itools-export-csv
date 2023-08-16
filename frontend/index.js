7b40f550723fad792c52a27e732da71bb086405d
Diego Guerrero <105182864+iseka1@users.noreply.github.com>
2023-08-16 11:10:53 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -25,8 +25,8 @@ function MyBlock() {
 
     const exportToCSV = async () => {
         const months = [
-            'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
-            'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+            'EneroF1','EneroF2', 'FebreroF1','FebreroF2', 'MarzoF1','MarzoF2', 'AbrilF1','AbrilF2', 'MayoF1','MayoF2', 'JunioF1','JunioF2',
+            'JulioF1','JulioF2', 'AgostoF1','AgostoF2', 'SeptiembreF1','SeptiembreF2', 'OctubreF1','OctubreF2', 'NoviembreF1','NoviembreF2', 'DiciembreF1','DiciembreF2'
         ];
 
         // Create a mapping of Medusa to Team data
@@ -38,31 +38,45 @@ function MyBlock() {
                 medusaTeamMap[medusa] = {
                     rol: record.getCellValueAsString('Charge for Pipeline (from People) (from Medusa)'),
                     team: team,
-                    months: new Array(12).fill('')
+                    months: new Array(24).fill('')
                 };
             }
 
             const since = record.getCellValue('MonthSince');
             const until = record.getCellValue('MonthUntil');
-
+            const daySince = record.getCellValue('daySinceTeam');
+            const dayUntil = record.getCellValue('dayUntilTeam');
+    
             for (let month = since; month <= until; month++) {
+
+                //if else code
                 medusaTeamMap[medusa].months[month - 1] = team;
+
             }
         });
 
+
+        //insert vacaction en el array
         recordsVacations.forEach(vacation => {
             const medusa = vacation.getCellValueAsString('Medusa');
             const since = vacation.getCellValue('MonthSince');
-            const until = vacation.getCellValue('MonthUntil');
+            const until = vacation.getCellValue('MonthUntil');        
+            const daySince = vacation.getCellValue('daySinceVacations');
+            const dayUntil = vacation.getCellValue('dayUntilVacations');
+    
 
             for (let month = since; month <= until; month++) {
                 const teamData = medusaTeamMap[medusa];
                 if (teamData) {
+
+                    //if else code
                     teamData.months[month - 1] = 'Vacation';
                 }
             }
         });
 
+
+        //Export a csv
         const csvData = [
             ['Medusa', 'Rol', ...months], // Add header row
             ...Object.entries(medusaTeamMap).map(([medusa, data]) => {
